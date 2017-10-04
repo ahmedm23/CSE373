326ccb687154f2bc2ec00c0bd38d9d713687a67c
Ahmed Moalim <ahmedm23@uw.edu>
2017-10-04 13:23:29 -0700

    Implemented ArrayDictionary Class

--- project1-master-2e26038fd77b3107cc6f17f8d035b7698bfe37cb/src/main/java/datastructures/concrete/dictionaries/ArrayDictionary.java
+++ project1-master-2e26038fd77b3107cc6f17f8d035b7698bfe37cb/src/main/java/datastructures/concrete/dictionaries/ArrayDictionary.java
@@ -1,8 +1,9 @@
 package datastructures.concrete.dictionaries;
 
+import java.util.Arrays;
+
 import datastructures.interfaces.IDictionary;
 import misc.exceptions.NoSuchKeyException;
-import misc.exceptions.NotYetImplementedException;
 
 /**
  * See IDictionary for more details on what this class should do
@@ -11,11 +12,13 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
     // You may not change or rename this field: we will be inspecting
     // it using our private tests.
     private Pair<K, V>[] pairs;
-
+    private int size;
+    
     // You're encouraged to add extra fields (and helper methods) though!
 
     public ArrayDictionary() {
-        throw new UnsupportedOperationException();
+    		size = 0;
+        pairs = makeArrayOfPairs(10);
     }
 
     /**
@@ -42,27 +45,76 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
 
     @Override
     public V get(K key) {
-        throw new NotYetImplementedException();
+	    	return pairs[findIndex(key)].value;
     }
 
     @Override
     public void put(K key, V value) {
-        throw new NotYetImplementedException();
+    		Pair<K, V> pair = new Pair<K, V>(key, value);
+    		
+    		try {
+    			int index = findIndex(key);
+    			pairs[index] = pair;
+    			
+    		} catch (NoSuchKeyException e) {
+    			if (size  >= pairs.length) {
+    				Pair<K, V>[] largerPairs = makeArrayOfPairs(pairs.length * 2); 
+    				System.arraycopy(pairs, 0, largerPairs, 0, pairs.length);
+    				pairs = largerPairs;
+        		}
+        		
+        		pairs[size] = pair;
+        		size++;
+    		}
+    }
+    
+    /**
+     * Returns the index corresponding to the given key.
+     *
+     *  @throws NoSuchKeyException if the dictionary does not contain the given key.
+     *  @throws NullPointerException if the dictionary is empty.
+     */
+    private int findIndex(K key) {
+    		for(int i = 0; i < size; i++) {
+	    		if (pairs[i].key == key || pairs[i].key.equals(key))
+	    			return i;
+	    }
+    		
+    		throw new NoSuchKeyException();
     }
 
     @Override
     public V remove(K key) {
-        throw new NotYetImplementedException();
+    		int index = findIndex(key);
+    		V value = pairs[index].value;
+    		
+    		for(int i = index; i < size - 1; i++) {
+    			pairs[i] = pairs[i+1];
+    		}
+    		
+    		pairs[size - 1] = null;
+    		size--;
+    		return value;
     }
 
     @Override
     public boolean containsKey(K key) {
-        throw new NotYetImplementedException();
+    		try {
+			findIndex(key);
+			return true;	
+		} catch (NoSuchKeyException e) {
+			return false;
+    		}
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return size;
+    }
+    
+    @Override
+    public String toString() {
+        return Arrays.toString(pairs);
     }
 
     private static class Pair<K, V> {
